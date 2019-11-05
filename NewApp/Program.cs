f277efed90ccc7370fe8586ca2b96ca5dea9244d
sayantanisarkar <sarkar.sayantani@gmail.com>
2019-11-05 18:03:35 +0530

    First Commit for GitHub

---
+++ NewApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NewApp
+{
+    // public class Person
+    // {
+    //     public string FName {get;set;}
+    //     public string LName {get; set;}
+
+    //     public Person(string fname,string lname)
+    //     {
+    //         FName = fname;
+    //         LName = lname;
+    //     }
+
+    //     public override string ToString()
+    //     {
+    //         return FName + " " + LName;
+    //     }
+
+    //     public string ToCapital()
+    //     {
+    //         return ToString().ToUpper();
+    //     }
+
+    // }
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+           // Console.WriteLine("Hello World!Hello World!");
+           // Console.WriteLine("Current Time : " + DateTime.Now);
+
+           //var p = new Person("Abdul", "Kalam");
+          // Console.WriteLine("Person's Name : " + p.ToCapital());
+          // Console.WriteLine("Person's Name in Upper: " + p.ToCapital());
+        Console.WriteLine(nameof(System.String));
+        int j = 5;
+        Console.WriteLine(nameof(j));
+        List<string> names = new List<string>();
+        Console.WriteLine(nameof(names));
+
+        }
+    }
+}
